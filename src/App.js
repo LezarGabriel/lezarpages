94c8717c358d06e99c7edc085be7614a01a683f2
DESKTOP-C3J5RM7\LENOVO <lezargabrieldarapai@gmail.com>
2023-01-14 13:57:03 +0700

    change branch text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome To My Page
+          Welcome To Development Branch
         </p>
         <a
           className="App-link"
